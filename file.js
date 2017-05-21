d1e535b67d17c37057c2a076cc3ac61ea54bab47
Brian Park <lucid@Brians-MBP.hitronhub.home>
2017-05-20 22:56:12 -0700

    Revert "change 3"
    This reverts commit ef21027e5cef802e6670c311b8c458ed30b91e3b.

--- file.js
+++
@@ -1,3 +0,0 @@
-export function file () {
-  return 'file'
-}
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const function firstCommmit () {
   const 'change 1'
   const 'change 2'
-  const 'change 3'
   return 'first commit'
 }
